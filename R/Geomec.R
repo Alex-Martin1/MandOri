3c54385f00986ece49f74b4ebeb6f1fbaf035448
Alex-Martin1 <almartan@ucm.es>
2024-05-23 16:22:09 +0100

    Standard mess change - "Mandibular superior length"

--- R/Geomec.R
+++ R/Geomec.R
@@ -95,10 +95,10 @@ distR <- function(input_text) {
     "Corpus length" = sqrt(sum((est_distances_matrix[1, ] - est_distances_matrix[11, ])^2)),
     "Ramus lateral length" = sqrt(sum((est_distances_matrix[6, ] - est_distances_matrix[13, ])^2)),
     "Ramus width" = sqrt(sum((est_distances_matrix[10, ] - est_distances_matrix[21, ])^2)),
-    "Dental arch breadth" = sqrt(sum((est_distances_matrix[2, ] - est_distances_matrix[22, ])^2))
+    "Mandibular superior length" = sqrt(sum((est_distances_matrix[2, ] - est_distances_matrix[22, ])^2))
   )
   Estandardization_distances <- matrix(distances, nrow = 1, ncol = 4)
-  colnames(Estandardization_distances) <- c("Corpus length", "Ramus lateral length", "Ramus width", "Dental arch breadth") 
+  colnames(Estandardization_distances) <- c("Corpus length", "Ramus lateral length", "Ramus width", "Mandibular superior length") 
   assign("Estandardization_distances", Estandardization_distances, envir = .GlobalEnv)
   return(Estandardization_distances)
 }
@@ -109,10 +109,10 @@ CalcEstDistances <- function(input_matrix) {
     "Corpus length" = sqrt(sum((input_matrix[3, ] - input_matrix[5, ])^2)),
     "Ramus lateral length" = sqrt(sum((input_matrix[6, ] - input_matrix[7, ])^2)),
     "Ramus width" = sqrt(sum((input_matrix[8, ] - input_matrix[9, ])^2)),
-    "Dental arch breadth" = sqrt(sum((input_matrix[2, ] - input_matrix[11, ])^2))
+    "Mandibular superior length" = sqrt(sum((input_matrix[2, ] - input_matrix[11, ])^2))
   )
   Estandardization_distances <- matrix(distances, nrow = 1, ncol = 4)
-  colnames(Estandardization_distances) <- c("Corpus length", "Ramus lateral length", "Ramus width", "Dental arch breadth") 
+  colnames(Estandardization_distances) <- c("Corpus length", "Ramus lateral length", "Ramus width", "Mandibular superior length") 
   assign("Estandardization_distances", Estandardization_distances, envir = .GlobalEnv)
   return(Estandardization_distances)
 }
@@ -144,7 +144,7 @@ cross_product <- function(u_vector, v_vector) {
 text2matrix <- function(input_text) {
   values <- strsplit(input_text, "\\s+")
   if (length(unlist(values)) %% 3 != 0) {
-    stop("The number of values must be a multiple of 3.")
+    stop("The number of values must be a multiple of 3")
   }
   values <- as.numeric(unlist(values))
   result_matrix <- matrix(values, ncol = 3, byrow = TRUE)
